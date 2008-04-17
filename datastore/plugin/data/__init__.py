4b43f7807fbb66e0c945a174ee893bab5b3e28bc
No Author <noauthor@water.ca.gov>
2008-04-17 20:56:02 +0000

    git-svn-id: http://dminfo:8686/svn/repository/vtools/trunk/vtools/vtools@370 cdc4813b-4270-ec4f-936e-925f93a782c2

--- datastore/plugin/data/__init__.py
+++ datastore/plugin/data/__init__.py
@@ -1,14 +1,12 @@
-#
-# UrbanSim software. Copyright (C) 1998-2007 University of Washington
-# 
-# You can redistribute this program and/or modify it under the terms of the
-# GNU General Public License as published by the Free Software Foundation
-# (http://www.gnu.org/copyleft/gpl.html).
-# 
-# This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-# FITNESS FOR A PARTICULAR PURPOSE. See the file LICENSE.html for copyright
-# and licensing information, and the file ACKNOWLEDGMENTS.html for funding and
-# other acknowledgments.
-# 
+#------------------------------------------------------------------------------
+# Copyright (c) 2007 by Enthought, Inc.
+# All rights reserved.
+#------------------------------------------------------------------------------
+
+try:
+    __import__('pkg_resources').declare_namespace(__name__)
+except:
+    pass
+
+
 
