55fe98a451b7df36bea046e1344fa4f5c5a7d72f
No Author <noauthor@water.ca.gov>
2008-09-25 21:16:09 +0000

    deactivate dss resources import in dss.api for the moment
    git-svn-id: http://dminfo:8686/svn/repository/vtools/trunk@436 cdc4813b-4270-ec4f-936e-925f93a782c2

--- vtools.datastore.dss/vtools/datastore/dss/api.py
+++ vtools.datastore.dss/vtools/datastore/dss/api.py
@@ -2,4 +2,6 @@ from dss_catalog import *
 from dss_constants import *
 from dss_service import *
 from utility import *
-from dss_resource_type import *
+
+## deactived it for the moment
+##from dss_resource_type import *
