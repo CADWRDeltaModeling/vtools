c6a5724dc90259541a03004fd5ac954050174c04
Jon Shu <qshu@water.ca.gov>
2013-11-06 16:53:03 +0000

    git-svn-id: http://dminfo:8686/svn/repository/vtools/trunk/vtools/vtools@2338 cdc4813b-4270-ec4f-936e-925f93a782c2

---
+++ setup.py
@@ -0,0 +1,40 @@
+
+"""vtools setup file"""
+
+import ez_setup
+ez_setup.use_setuptools()
+from setuptools import setup,find_packages
+
+# Metadata
+PACKAGE_NAME = "vtools"
+PACKAGE_VERSION = "0.1.6"
+PACKAGES = ['datastore','data','examples','functions','graph']
+
+setup(
+    name=PACKAGE_NAME,
+    version=PACKAGE_VERSION,
+    url = "http://bdo.water.ca.gov",
+    description='time series manipulation tools',
+    #long_description = get_description(),
+    author="Eli Ateljevich, Qiang Shu",
+    author_email="eli@water.ca.gov",
+    license="PSF or ZPL",
+    install_requires = ['setuptools','Traits>=3.4','Chaco>3.3','gadfly'],
+    packages=find_packages(),
+    namespace_packages=['vtools','vtools.datastore'],
+
+    package_data={"":["doc/*.*","doc/html/*.*","*.pyd"],
+                  "vtools.examples.graph":["*.txt"],
+                  "vtools.graph":["formats/*.txt"]
+                 },
+    exclude_package_data = {'': ['*.pyc','*.pyo']},
+    entry_points = {
+        'console_scripts': [
+            'transfer = vtools.datastore.transfer:main'
+        ]},
+      test_suite = "nose.collector",
+      tests_require = "nose"        
+        
+)
+
+print find_packages()
