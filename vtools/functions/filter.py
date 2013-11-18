ab8825b61c60337e793155ec518633f10122842e
Jon Shu <qshu@water.ca.gov>
2013-11-18 21:21:55 +0000

    combine godin filter into filter, remove tidal_filter
    git-svn-id: http://dminfo:8686/svn/repository/vtools/trunk@2422 cdc4813b-4270-ec4f-936e-925f93a782c2

--- vtools/functions/filter.py
+++ vtools/functions/filter.py
@@ -16,7 +16,7 @@ from vtools.data.vtime import *
 from vtools.data.timeseries import rts
 from vtools.data.constants import *
 
-__all__=["boxcar","butterworth","daily_average"]
+__all__=["boxcar","butterworth","daily_average","godin"]
 
 
 
@@ -162,6 +162,48 @@ def daily_average(ts):
     before=parse_interval("1day")
     after=parse_interval("0min")   
     return boxcar(ts,before,after)
+    
+    
+## This dic saves the points involves in half summations 
+## for different intervals
+godin_interval={time_interval(minutes=15):(48,49),\
+                time_interval(hours=1):(12,12)} 
+
+
+def godin(ts):
+    
+    """ Godin filtering method on a regular time series.
+        input:
+            ts: regular time series to be filtered
+        output:
+            new regular time series with the same interval of ts.
+    """
+    
+    if not ts.is_regular():
+        raise ValueError("Only regular time series can be filtered.")
+    
+    interval=ts.interval
+    
+    if not interval in godin_interval.keys():
+        raise ValueError("Not supported time interval by godin filter")
+    
+    num_1=godin_interval[interval][0]
+    num_2=godin_interval[interval][1]   
+    ## use boxcar averging fuction 3 times to 
+    ## achieve final values.
+    
+    d1=_boxcar(ts.data,num_1-1,num_1)
+    d1=_boxcar(d1,num_1,num_1-1)
+    d1=_boxcar(d1,num_2,num_2)
+    
+    prop={}
+    for key,val in ts.props.items():
+        prop[key]=val
+    prop[TIMESTAMP]=INST
+    prop[AGGREGATION]=INDIVIDUAL
+        
+    return rts(d1,ts.start,ts.interval,prop)
+
         
 ########################################################################### 
 ## Private interface.
--- vtools/functions/test_filter.py
+++ vtools/functions/test_filter.py
@@ -147,6 +147,97 @@ class TestFilter(unittest.TestCase):
         test_ts=rts(data,st,delta,{})
         nt3=daily_average(test_ts)
         
+    def test_godin(self):
+        """ test a godin filter on a series of 1hour interval with four
+            frequencies.
+        """
+        # Test operations on ts of varied values.
+        test_ts=[(datetime.datetime(year=1990,month=2,day=3,hour=11, minute=15),\
+                  int(scipy.math.pow(2,10)),time_interval(hours=1)),]
+
+        f1=0.76
+        f2=0.44
+        f3=0.95
+        f4=1.23
+        av1=f1*scipy.pi/12
+        av2=f2*scipy.pi/12
+        av3=f3*scipy.pi/12
+        av4=f4*scipy.pi/12
+                 
+        for (st,num,delta) in test_ts:
+            ## this day contains components of with frequecies of 0.76/day,
+            ## 0.44/day, 0.95/day, 1.23/day            
+            data=[scipy.math.sin(av1*k)+0.7*scipy.math.cos(av2*k)+2.4*scipy.math.sin(av3*k) 
+                  +0.1*scipy.math.sin(av4*k) for k in range(num)] 
+                                
+            # This ts is the orignial one.           
+            ts0=rts(data,st,delta,{})   
+            ts=godin(ts0)
+            self.assert_(ts.is_regular())
+            
+    def test_godin_15min(self):        
+        """ test godin filtering on a 15min constant values 
+            data series with a nan.
+        """        
+        data=[1.0]*800+[2.0]*400+[1.0]*400
+        data=scipy.array(data)
+        data[336]=scipy.nan
+        st=datetime.datetime(year=1990,month=2,day=3,hour=11, minute=15)
+        delta=time_interval(minutes=15)
+        test_ts=rts(data,st,delta,{})
+        nt3=godin(test_ts)
+        self.assert_(scipy.alltrue(scipy.isnan(nt3.data[0:144])))
+        assert_array_almost_equal(nt3.data[144:192],[1]*48,12)
+        self.assert_(scipy.alltrue(scipy.isnan(nt3.data[192:481])))
+        assert_array_almost_equal(nt3.data[481:656],[1]*175,12)
+        self.assert_(scipy.alltrue(scipy.greater(nt3.data[656:944],1)))
+        self.assertAlmostEqual(nt3.data[868],1.916618441)
+        assert_array_almost_equal(nt3.data[944:1056],[2]*112,12)
+        self.assert_(scipy.alltrue(scipy.greater(nt3.data[1056:1344],1)))
+        self.assertAlmostEqual(nt3.data[1284],1.041451845)
+        assert_array_almost_equal(nt3.data[1344:1456],[1]*112,12)
+        self.assert_(scipy.alltrue(scipy.isnan(nt3.data[1456:1600])))   
+                                   
+    def test_godin_2d(self):
+        
+        """ Test godin filter on 2-dimensional data set."""
+        
+        d1=[1.0]*800+[2.0]*400+[1.0]*400
+        d2=[1.0]*800+[2.0]*400+[1.0]*400
+        data=scipy.array([d1,d2])
+        data=scipy.transpose(data)
+        data[336,:]=scipy.nan
+        st=datetime.datetime(year=1990,month=2,day=3,hour=11, minute=15)
+        delta=time_interval(minutes=15)
+        test_ts=rts(data,st,delta,{})
+        
+        nt3=godin(test_ts)
+        d1=nt3.data[:,0]
+        d2=nt3.data[:,1]
+        self.assert_(scipy.alltrue(scipy.isnan(d1[0:144])))
+        assert_array_almost_equal(d1[144:192],[1]*48,12)
+        self.assert_(scipy.alltrue(scipy.isnan(d1[192:481])))
+        assert_array_almost_equal(d1[481:656],[1]*175,12)
+        self.assert_(scipy.alltrue(scipy.greater(d1[656:944],1)))
+        self.assertAlmostEqual(d1[868],1.916618441)
+        assert_array_almost_equal(d1[944:1056],[2]*112,12)
+        self.assert_(scipy.alltrue(scipy.greater(d1[1056:1344],1)))
+        self.assertAlmostEqual(d1[1284],1.041451845)
+        assert_array_almost_equal(d1[1344:1456],[1]*112,12)
+        self.assert_(scipy.alltrue(scipy.isnan(d1[1456:1600]))) 
+        
+        self.assert_(scipy.alltrue(scipy.isnan(d2[0:144])))
+        assert_array_almost_equal(d2[144:192],[1]*48,12)
+        self.assert_(scipy.alltrue(scipy.isnan(d2[192:481])))
+        assert_array_almost_equal(d2[481:656],[1]*175,12)
+        self.assert_(scipy.alltrue(scipy.greater(d2[656:944],1)))
+        self.assertAlmostEqual(d2[868],1.916618441)
+        assert_array_almost_equal(d2[944:1056],[2]*112,12)
+        self.assert_(scipy.alltrue(scipy.greater(d2[1056:1344],1)))
+        self.assertAlmostEqual(d2[1284],1.041451845)
+        assert_array_almost_equal(d2[1344:1456],[1]*112,12)
+        self.assert_(scipy.alltrue(scipy.isnan(d2[1456:1600]))) 
+    
               
 if __name__=="__main__":
     
