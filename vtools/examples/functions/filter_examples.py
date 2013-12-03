7c829c5b9242bf91b16e31a40d788f187800feed
Jon Shu <qshu@water.ca.gov>
2013-12-03 18:03:10 +0000

    add some working examples, they uses vtools sample data
    git-svn-id: http://dminfo:8686/svn/repository/vtools/trunk@2461 cdc4813b-4270-ec4f-936e-925f93a782c2

---
+++ vtools/examples/functions/filter_examples.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from vtools.functions.api import *
+from vtools.data.api import *
+import matplotlib.pyplot as plt
+
+
+
+ts_1day=example_data("pt_reyes_tidal_6min")
+ts_butt=butterworth(ts_1day,cutoff_period="1hour")
+boxcar_aver_interval = parse_interval("1hour")
+ts_box=boxcar(ts_1day,boxcar_aver_interval,boxcar_aver_interval)
+fig=plt.figure()
+ax0 = fig.add_subplot(111)
+ax0.set_ylabel("surface (feet)")
+p0=ax0.plot(ts_1day.times,ts_1day.data,color='g',linewidth=1.2)
+p1=ax0.plot(ts_butt.times,ts_butt.data,color='r',linewidth=1.)
+p2=ax0.plot(ts_box.times,ts_box.data,color='b',linewidth=0.5)
+plt.legend(["Surface","Butterworth","Boxcar"])
+plt.grid(b=True, which='major', color='0.9', linestyle='-', linewidth=0.5)
+fig.autofmt_xdate()
+plt.show()
+
---
+++ vtools/examples/functions/tidal_examples.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+
+from vtools.functions.api import *
+from vtools.data.api import *
+import matplotlib.pyplot as plt
+
+
+
+ts_1week=example_data("pt_reyes_tidal_1hour")
+ts_butt=butterworth(ts_1week,cutoff_period="30hour")
+boxcar_aver_interval = parse_interval("25hour")
+ts_box=boxcar(ts_1week,boxcar_aver_interval,boxcar_aver_interval)
+ts_god=godin(ts_1week)
+fig=plt.figure()
+ax0 = fig.add_subplot(111)
+ax0.set_ylabel("surface (feet)")
+p0=ax0.plot(ts_1week.times,ts_1week.data,color='green',linewidth=1.2)
+p1=ax0.plot(ts_butt.times,ts_butt.data,color='red',linewidth=0.5)
+p2=ax0.plot(ts_box.times,ts_box.data,color='blue',linewidth=0.5)
+p2=ax0.plot(ts_god.times,ts_god.data,color='black',linewidth=0.5)
+plt.legend(["Surface","Butterworth","Boxcar","Godin"])
+plt.grid(b=True, which='both', color='0.9', linestyle='-', linewidth=0.5)
+fig.autofmt_xdate()
+plt.show()
