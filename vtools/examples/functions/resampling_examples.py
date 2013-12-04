e085e709b361b7dba2fc21a2daa4e14db9b89d0b
Jon Shu <qshu@water.ca.gov>
2013-12-04 18:20:00 +0000

    git-svn-id: http://dminfo:8686/svn/repository/vtools/trunk@2468 cdc4813b-4270-ec4f-936e-925f93a782c2

---
+++ vtools/examples/functions/resampling_examples.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from vtools.functions.api import *
+from vtools.data.api import *
+import matplotlib.pyplot as plt
+
+
+ts=synthetic_tide_series()
+ts_resample=decimate(ts,"1hour")
+fig=plt.figure()
+ax0 = fig.add_subplot(111)
+ax0.set_ylabel("surface (feet)")
+p0=ax0.plot(ts.times,ts.data,color='g',linewidth=1.2)
+p1=ax0.plot(ts_resample.times,ts_resample.data,color='r',marker=".",linestyle="none",markersize=3)
+plt.legend(["Surface","Decimated"])
+plt.grid(b=True, which='major', color='0.9', linestyle='-', linewidth=0.5)
+fig.autofmt_xdate()
+plt.show()
+
---
+++ vtools/examples/functions/shifting_examples.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from vtools.functions.api import *
+from vtools.data.api import *
+import matplotlib.pyplot as plt
+
+
+ts=synthetic_tide_series()
+ts_shifted=shift(ts,"1day")
+fig=plt.figure()
+ax0 = fig.add_subplot(111)
+ax0.set_ylabel("surface (feet)")
+p0=ax0.plot(ts.times,ts.data,color='g',linewidth=1.0)
+p1=ax0.plot(ts_shifted.times,ts_shifted.data,color='r',linewidth=1.0)
+plt.legend(["Surface","Shifted"])
+plt.grid(b=True, which='major', color='0.9', linestyle='-', linewidth=0.5)
+fig.autofmt_xdate()
+plt.show()
+
