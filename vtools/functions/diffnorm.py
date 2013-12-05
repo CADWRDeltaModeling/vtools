3c9c933756aa353828b8d63cc7ebce8ed6b06b03
Jon Shu <qshu@water.ca.gov>
2013-12-05 00:00:32 +0000

    git-svn-id: http://dminfo:8686/svn/repository/vtools/trunk@2478 cdc4813b-4270-ec4f-936e-925f93a782c2

--- vtools/functions/diffnorm.py
+++ vtools/functions/diffnorm.py
@@ -1,4 +1,5 @@
-"""  uility to compute ts norm (L_1,L_2,L_inf)
+"""  uility to compute ts norm (L_1,L_2,L_inf).
+
 """
 import numpy
 
@@ -10,15 +11,14 @@ __all__=["norm_diff_l1","norm_diff_l2","norm_diff_linf","ts_equal","ts_almost_eq
 
 
 def _norm_L1_array(data):
-    """
-        estimate L_1 norm of input numpy array data
+    """Estimate L_1 norm of input numpy array data.
+    
     """
     data=numpy.abs(data)
     return data.sum()
     
 def _norm_L2_array(data):
-    """
-        estimate L_2 norm of input numpy array data
+    """Estimate L_2 norm of input numpy array data.
     """
     data=numpy.square(data)
     val=data.sum()
@@ -27,14 +27,36 @@ def _norm_L2_array(data):
 
 
 def _norm_Linf_array(data):
-    """
-        estimate L_inf norm of input numpy array data
+    """Estimate L_inf norm of input numpy array data.
+    
     """
     data=numpy.abs(data)
     return numpy.maximum.reduce(data)
     
     
 def norm_diff_l1(ts1,ts2,window=None):
+    """ Compute L1 difference of two input time series.
+
+    Parameters
+    -----------
+    ts1 : :class:`~vtools.data.timeseries.TimeSeries`
+           Must has data of one dimension, and regular.
+    
+    ts2 : :class:`~vtools.data.timeseries.TimeSeries`
+           Must has data of one dimension, and regular.
+    
+    window :Optional, tuple
+           Tupleof two :class:String representing time, or Tuple
+           of two :class:Datetime.Datetime.
+           
+
+    Returns
+    -------
+    result : float
+        A value of total L1-difference between two input series .
+        
+    """
+
     if window is None:
         window=(ts1.start,ts1.end)
     st=window[0]
@@ -44,6 +66,27 @@ def norm_diff_l1(ts1,ts2,window=None):
     
     
 def norm_diff_l2(ts1,ts2,window=None):
+    """ Compute L2 difference of two input time series.
+
+    Parameters
+    -----------
+    ts1 : :class:`~vtools.data.timeseries.TimeSeries`
+           Must has data of one dimension, and regular.
+    
+    ts2 : :class:`~vtools.data.timeseries.TimeSeries`
+           Must has data of one dimension, and regular.
+    
+    window :Optional, tuple
+           Tuple of two :class:String representing time, or tuple
+           of two :class:Datetime.Datetime.
+           
+
+    Returns
+    -------
+    result : float
+        A value of total L2-difference between two input series .
+        
+    """
     if window is None:
         window=(ts1.start,ts1.end)
     st=window[0]
@@ -54,6 +97,27 @@ def norm_diff_l2(ts1,ts2,window=None):
     
     
 def norm_diff_linf(ts1,ts2,window=None):
+    """ Compute L_inf difference of two input time series.
+
+    Parameters
+    -----------
+    ts1 : :class:`~vtools.data.timeseries.TimeSeries`
+           Must has data of one dimension, and regular.
+    
+    ts2 : :class:`~vtools.data.timeseries.TimeSeries`
+           Must has data of one dimension, and regular.
+    
+    window :Optional, tuple
+           Tuple of two :class:String representing time, or Tuple
+           of two :class:Datetime.Datetime.
+           
+
+    Returns
+    -------
+    result : float
+        A value of total L_infinite difference between two input series .
+        
+    """
     if window is None:
         window=(ts1.start,ts1.end)
     st=window[0]
@@ -62,8 +126,27 @@ def norm_diff_linf(ts1,ts2,window=None):
     return _norm_Linf_array(diffts.data)
             
 def ts_equal(ts1,ts2,window=None,tol=0.0e0):
-    """
-      compare two ts by absolute difference
+    """Compare two ts by absolute difference.
+
+    Parameters
+    -----------
+    ts1 : :class:`~vtools.data.timeseries.TimeSeries`
+           Must has data of one dimension, and regular.
+    
+    ts2 : :class:`~vtools.data.timeseries.TimeSeries`
+           Must has data of one dimension, and regular.
+    
+    window :Optional, tuple
+           Tupleof two :class:String representing time, or Tuple
+           of two :class:Datetime.Datetime.
+           
+    tol    :Optional, float
+    
+    Returns
+    -------
+    result : bool
+        If equal within allowed tolerance, return True .
+        
     """
     if window is None:
         window=(ts1.start,ts1.end)
@@ -78,8 +161,27 @@ def ts_equal(ts1,ts2,window=None,tol=0.0e0):
         return False
         
 def ts_almost_equal(ts1,ts2,window=None,tol=1.0e-8):
-    """
-       compare two ts by normalized difference
+    """Compare two ts by absolute difference.
+
+    Parameters
+    -----------
+    ts1 : :class:`~vtools.data.timeseries.TimeSeries`
+           Must has data of one dimension, and regular.
+    
+    ts2 : :class:`~vtools.data.timeseries.TimeSeries`
+           Must has data of one dimension, and regular.
+    
+    window :Optional, tuple
+           Tuple of two :class:String representing time, or tuple
+           of two :class:Datetime.Datetime.
+           
+    tol    :Optional, float
+    
+    Returns
+    -------
+    result : bool
+        If equal within allowed tolerance, return True .
+        
     """
     if window is None:
         window=(ts1.start,ts1.end)
