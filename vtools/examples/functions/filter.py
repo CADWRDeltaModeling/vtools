25c3347489fbc41248c8bad5707b9ef11dc4e456
Jon Shu <qshu@water.ca.gov>
2014-01-02 23:07:49 +0000

    git-svn-id: http://dminfo:8686/svn/repository/vtools/trunk@2514 cdc4813b-4270-ec4f-936e-925f93a782c2

--- vtools/examples/functions/filter.py
+++ vtools/examples/functions/filter.py
@@ -1,26 +1,19 @@
-
-
 from vtools.functions.filter import *
 from vtools.data.sample_series import *
 import matplotlib.pyplot as plt
 
-
-
-ts_1week=example_data("pt_reyes_tidal_1hour")
-ts_butt=butterworth(ts_1week,cutoff_period=hours(30))
-boxcar_aver_interval = hours(25)
-ts_box=boxcar(ts_1week,boxcar_aver_interval,boxcar_aver_interval)
-ts_god=godin(ts_1week)
+ts_1day=example_data("pt_reyes_tidal_6min")
+ts_butt=butterworth(ts_1day,cutoff_period="1hour")
+boxcar_aver_interval = parse_interval("1hour")
+ts_box=boxcar(ts_1day,boxcar_aver_interval,boxcar_aver_interval)
 fig=plt.figure()
 ax0 = fig.add_subplot(111)
 ax0.set_ylabel("surface (feet)")
-p0=ax0.plot(ts_1week.times,ts_1week.data,color='green',linewidth=1.2)
-p1=ax0.plot(ts_butt.times,ts_butt.data,color='red',linewidth=0.5)
-p2=ax0.plot(ts_box.times,ts_box.data,color='blue',linewidth=0.5)
-p2=ax0.plot(ts_god.times,ts_god.data,color='black',linewidth=0.5)
-plt.legend(["Surface","Butterworth","Boxcar","Godin"])
-plt.grid(b=True, which='both', color='0.9', linestyle='-', linewidth=0.5)
+p0=ax0.plot(ts_1day.times,ts_1day.data,color='g',linewidth=1.2)
+p1=ax0.plot(ts_butt.times,ts_butt.data,color='r',linewidth=1.)
+p2=ax0.plot(ts_box.times,ts_box.data,color='b',linewidth=0.5)
+plt.legend(["Surface","Butterworth","Boxcar"])
+plt.grid(b=True, which='major', color='0.9', linestyle='-', linewidth=0.5)
 fig.autofmt_xdate()
 plt.show()
-
 
