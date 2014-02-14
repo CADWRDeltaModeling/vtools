c1c6887c7b46da15ba370f458974a88f0574e606
Jon Shu <qshu@water.ca.gov>
2014-02-14 23:24:12 +0000

    add cosine-lanczos example
    git-svn-id: http://dminfo:8686/svn/repository/vtools/trunk@2538 cdc4813b-4270-ec4f-936e-925f93a782c2

--- vtools/examples/functions/tidal.py
+++ vtools/examples/functions/tidal.py
@@ -4,22 +4,28 @@
 from vtools.functions.filter import *
 from vtools.data.sample_series import *
 import matplotlib.pyplot as plt
+import datetime
 
-
-
-ts_1week=example_data("pt_reyes_tidal_1hour")
-ts_butt=butterworth(ts_1week,cutoff_period=hours(36))
-boxcar_aver_interval = hours(25)
-ts_box=boxcar(ts_1week,boxcar_aver_interval,hours(25))
-ts_god=godin(ts_1week)
+ts=example_data("pt_reyes_tidal_1hour")
+## only plot part of result 
+window_start=datetime.datetime(2013,11,25)
+window_end=datetime.datetime(2013,12,2)
+ts_lowpass=cosine_lanczos(ts,cutoff_period=hours(30),M=30).window(window_start,window_end)
+ts_butt=butterworth(ts,cutoff_period=hours(60),order=4).window(window_start,window_end)
+boxcar_aver_interval = hours(12)
+ts_box=boxcar(ts,boxcar_aver_interval,boxcar_aver_interval).window(window_start,window_end)
+ts_god=godin(ts).window(window_start,window_end)
 fig=plt.figure()
 ax0 = fig.add_subplot(111)
 ax0.set_ylabel("surface (feet)")
-p0=ax0.plot(ts_1week.times,ts_1week.data,color='green',linewidth=1.2)
-p1=ax0.plot(ts_butt.times,ts_butt.data,color='red',linewidth=0.5)
-p2=ax0.plot(ts_box.times,ts_box.data,color='blue',linewidth=0.5)
-p2=ax0.plot(ts_god.times,ts_god.data,color='black',linewidth=0.5)
-plt.legend(["Surface","Butterworth","Boxcar","Godin"])
-plt.grid(b=True, which='both', color='0.9', linestyle='-', linewidth=0.5)
+ts=ts.window(window_start,window_end)
+lwidth=0.8
+p0=ax0.plot(ts.times,ts.data,color='green',linewidth=1.2,label="Pt Reyes Water Level")
+p1=ax0.plot(ts_butt.times,ts_butt.data,color='red',linewidth=lwidth,label="Butterworth")
+p2=ax0.plot(ts_box.times,ts_box.data,color='blue',linewidth=lwidth,label="Boxcar")
+p3=ax0.plot(ts_god.times,ts_god.data,color='black',linewidth=lwidth,label="Godin")
+p4=ax0.plot(ts_lowpass.times,ts_lowpass.data,color='brown',linewidth=lwidth,label="Cosine-Lanczos")
+plt.grid(b=True, which='both', color='0.9', linestyle='-', linewidth=lwidth)
+plt.legend(loc="lower left",ncol=2)
 fig.autofmt_xdate()
 plt.show()
