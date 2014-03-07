9b5d5f86abf0a019f3a9dcac2f9306bf0b161e8a
Jon Shu <qshu@water.ca.gov>
2014-03-07 18:45:02 +0000

    format edits
    git-svn-id: http://dminfo:8686/svn/repository/vtools/trunk@2550 cdc4813b-4270-ec4f-936e-925f93a782c2

--- vtools/examples/functions/filter.py
+++ vtools/examples/functions/filter.py
@@ -12,10 +12,14 @@ fig=plt.figure()
 ax0 = fig.add_subplot(111)
 ax0.set_ylabel("surface (feet)")
 lwidth=0.8
-p0=ax0.plot(ts_1day.times,ts_1day.data,color='green',linewidth=lwidth,label="Surface")
-p1=ax0.plot(ts_butt.times,ts_butt.data,color='red',linewidth=lwidth,label="Butterworth")
-p2=ax0.plot(ts_box.times,ts_box.data,color='black',linewidth=lwidth,label="Boxcar")
-p3=ax0.plot(ts_cos.times,ts_cos.data,color='brown',linewidth=lwidth,label="Cosine")
+p0=ax0.plot(ts_1day.times,ts_1day.data,color='green',\
+            linewidth=lwidth,label="Surface")
+p1=ax0.plot(ts_butt.times,ts_butt.data,color='red',\
+            linewidth=lwidth,label="Butterworth")
+p2=ax0.plot(ts_box.times,ts_box.data,color='black',\
+            linewidth=lwidth,label="Boxcar")
+p3=ax0.plot(ts_cos.times,ts_cos.data,color='brown',\
+            linewidth=lwidth,label="Cosine")
 plt.legend(loc="lower center",prop={'size':12})
 plt.grid(b=True, which='major', color='0.9', linestyle='-', linewidth=0.5)
 fig.autofmt_xdate()
--- vtools/examples/functions/spring_neap_tide.py
+++ vtools/examples/functions/spring_neap_tide.py
@@ -1,6 +1,4 @@
 # -*- coding: utf-8 -*-
-
-
 from vtools.functions.filter import *
 from vtools.data.sample_series import *
 import matplotlib.pyplot as plt
@@ -16,9 +14,12 @@ ax0 = fig.add_subplot(111)
 ax0.set_ylabel("flow (cfs)")
 lwidth=0.8
 p0=ax0.plot(ts.times,ts.data,color='green',linewidth=1.2,label="Flow")
-p2=ax0.plot(ts_box.times,ts_box.data,color='blue',linewidth=lwidth,label="Boxcar")
-p3=ax0.plot(ts_box_god.times,ts_box_god.data,color='black',linewidth=lwidth,label="Boxcar+Godin")
-p4=ax0.plot(ts_lowpass.times,ts_lowpass.data,color='red',linewidth=lwidth,label="Cosine-Lanczos")
+p2=ax0.plot(ts_box.times,ts_box.data,color='blue',\
+            linewidth=lwidth,label="Boxcar")
+p3=ax0.plot(ts_box_god.times,ts_box_god.data,color='black',\
+            linewidth=lwidth,label="Boxcar+Godin")
+p4=ax0.plot(ts_lowpass.times,ts_lowpass.data,color='red',\
+            linewidth=lwidth,label="Cosine-Lanczos")
 plt.grid(b=True, which='both', color='0.9', linestyle='-', linewidth=lwidth)
 plt.legend(loc="lower left",ncol=2)
 fig.autofmt_xdate()
