3eee97bc1afe2f1458fcfd68914e899e7f71cdaa
Jon Shu <qshu@water.ca.gov>
2014-09-17 22:07:49 +0000

    git-svn-id: http://dminfo:8686/svn/repository/vtools/trunk@2695 cdc4813b-4270-ec4f-936e-925f93a782c2

--- vtools/examples/data/__init__.py
+++ vtools/examples/data/__init__.py
@@ -1,11 +1,5 @@
-#------------------------------------------------------------------------------
-# Copyright (c) 2007 by Enthought, Inc.
-# All rights reserved.
-#------------------------------------------------------------------------------
-
 try:
     __import__('pkg_resources').declare_namespace(__name__)
-except:
+except ImportError:
     pass
-
 
