2a3fd081f4131000d9a792bc36dbfde6c55b3bde
Jon Shu <qshu@water.ca.gov>
2015-03-02 21:47:09 +0000

    git-svn-id: http://dminfo:8686/svn/repository/vtools/branches/vtools_1.0@2794 cdc4813b-4270-ec4f-936e-925f93a782c2

--- py_build_win_inst.bat
+++
@@ -1,3 +0,0 @@
-if exist build. rmdir /Q /S build
-if exist dist. rmdir /Q /S dist
-python setup.py bdist --format=wininst 
--- vtools/datastore/dss/api.py
+++
@@ -1,7 +0,0 @@
-from dss_catalog import DssCatalogError,DssCatalog
-from dss_constants import *
-from dss_service import DssAccessError,DssCatalogServiceError,DssService
-from utility import *
-
-## deactived it for the moment
-##from dss_resource_type import *
