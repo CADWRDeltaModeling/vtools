Two minor bugs are fixed. 1. One index in extrapolate_ts had a wrong index with -1. 2. One line in _interpolate_ts2array was missing [0]'s.
git-svn-id: http://dminfo:8686/svn/repository/vtools/trunk@2813 cdc4813b-4270-ec4f-936e-925f93a782c2

vtools/data/timeseries.py
@@ -1003,7 +1003,7 @@ def extrapolate_ts(ts,start=None,end=None,method="constant",val=np.nan):
 
     if method=="constant":
         data[0:head_extended]=val
-        data[head_extended+old_len:new_len-1]=val
+        data[head_extended+old_len:new_len]=val
     elif method=="taper":
         if np.isnan(val):
             raise ValueError("You must input a valid value for taper method")