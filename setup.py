8c1f7367dd450574aa64f6655ed40ee36e974d92
Jon Shu <qshu@water.ca.gov>
2015-11-10 16:35:26 +0000

    increase minor version
    git-svn-id: http://dminfo:8686/svn/repository/vtools/branches/vtools_1.0@2989 cdc4813b-4270-ec4f-936e-925f93a782c2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup,find_packages
 
 # Metadata
 PACKAGE_NAME = "vtools"
-PACKAGE_VERSION = "1.0.3"
+PACKAGE_VERSION = "1.0.4"
 PACKAGES = ['datastore','data','examples','functions','datastore.dss']
 
 setup(
